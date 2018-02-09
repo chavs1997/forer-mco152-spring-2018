eb721db4c103bc84d741232081d8518fe891b245
chavs1997 <vforer@student.touro.edu>
2018-02-08 19:19:05 -0500

    commit corrected version of Vending Machine test

--- test/forer/Vending/VendingMachineTest.java
+++ test/forer/Vending/VendingMachineTest.java
@@ -21,62 +21,42 @@ class VendingMachineTest {
 	@Test
 	public void testPay2() {
 		VendingMachine vend = new VendingMachine();
-		int[] result = vend.pay(5.00, 4.11);
+		Change result = vend.pay(5.00, 4.11);
 
-		int numOfQuartersTest = result[0];
-		int numOfDimesTest = result[1];
-		int numOfNickelsTest = result[2];
-		int numOfPenniesTest = result[3];
-
-		assertEquals(3, numOfQuartersTest);
-		assertEquals(1, numOfDimesTest);
-		assertEquals(0, numOfNickelsTest);
-		assertEquals(4, numOfPenniesTest);
+		assertEquals(3, result.getNumOfQuarters());
+		assertEquals(1, result.getNumOfDimes());
+		assertEquals(0, result.getNumOfNickels());
+		assertEquals(4, result.getNumOfPennies());
 	}
 
 	@Test
 	public void testPay3() {
 		VendingMachine vend = new VendingMachine();
-		int[] result = vend.pay(3.00, 4.11);
+		Change result = vend.pay(3.00, 4.11);
 
-		int numOfQuartersTest = result[0];
-		int numOfDimesTest = result[1];
-		int numOfNickelsTest = result[2];
-		int numOfPenniesTest = result[3];
 
-		assertEquals(0, numOfQuartersTest);
-		assertEquals(0, numOfDimesTest);
-		assertEquals(0, numOfNickelsTest);
-		assertEquals(0, numOfPenniesTest);
+
 	}
 	
 	@Test
 	public void testDoublePay() {
 		VendingMachine vend = new VendingMachine();
-		int[] result = vend.pay(1.00, .99);
+		Change result = vend.pay(1.00, .99);
 
-		int numOfQuartersTest = result[0];
-		int numOfDimesTest = result[1];
-		int numOfNickelsTest = result[2];
-		int numOfPenniesTest = result[3];
 
-		assertEquals(0, numOfQuartersTest);
-		assertEquals(0, numOfDimesTest);
-		assertEquals(0, numOfNickelsTest);
-		assertEquals(1, numOfPenniesTest);
+		assertEquals(0, result.getNumOfQuarters());
+		assertEquals(0, result.getNumOfDimes());
+		assertEquals(0, result.getNumOfNickels());
+		assertEquals(1, result.getNumOfPennies());
 		
 		result = vend.pay(1.00, 1.00);
 		
 
-		numOfQuartersTest = result[0];
-		numOfDimesTest = result[1];
-		numOfNickelsTest = result[2];
-		numOfPenniesTest = result[3];
 
-		assertEquals(0, numOfQuartersTest);
-		assertEquals(0, numOfDimesTest);
-		assertEquals(0, numOfNickelsTest);
-		assertEquals(0, numOfPenniesTest);
+		assertEquals(0, result.getNumOfQuarters());
+		assertEquals(0, result.getNumOfDimes());
+		assertEquals(0, result.getNumOfNickels());
+		assertEquals(0, result.getNumOfPennies());
 	}
 
 }
