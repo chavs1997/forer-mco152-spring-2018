a54d4570792444d56705a51d669d03834f9b9502
chavs1997 <vforer@student.touro.edu>
2018-02-20 22:50:11 -0500

    changed dictionary code to HashMap instead of Scanner

--- src/forer/dictionary/Dictionary.java
+++ src/forer/dictionary/Dictionary.java
@@ -1,51 +1,52 @@
 package forer.dictionary;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Dictionary {
 
 	File dictionary;
+	Map<String, String> map;
 
 	public Dictionary() throws IOException {
 		File dictionary = new File(
 				"C:\\Users\\chavs\\Documents\\Eclipse Projects\\VendingMachine\\src\\forer\\Dictionary\\dictionary.txt");
 		this.dictionary = dictionary;
+		Map<String, String> map = new HashMap<String, String>();
+		BufferedReader in = new BufferedReader(new FileReader(dictionary));
+		String line = "";
+		while ((line = in.readLine()) != null) {
+			String[] parts = line.split(" ", 2);
+
+			if (parts.length < 2) {
+				String[] doubleParts = new String[2];
+				doubleParts[0] = parts[0];
+				doubleParts[1] = "null";
+				map.put(doubleParts[0], doubleParts[1]);
+
+			} else {
+
+				map.put(parts[0], parts[1]);
+
+			}
+		}
+		in.close();
+		this.map = map;
 	}
 
 	public boolean contains(String word) throws IOException {
-		BufferedReader dictionary = new BufferedReader(new FileReader(this.dictionary));
-		boolean contain = false;
-		String line;
-		while ((line = dictionary.readLine()) != null && contain == false) {
-			if (line.substring(0, word.length()).equalsIgnoreCase(word)
-					&& line.substring(word.length(), word.length() + 1).equalsIgnoreCase(" ")) {
-				contain = true;
-			}
 
-		}
-		dictionary.close();
-		return contain;
+		return map.containsKey(word.toUpperCase());
 	}
 
 	public String getDefinition(String word) throws IOException {
 		String definition = null;
-		Scanner sc = new Scanner(this.dictionary);
-		String line;
-		while (sc.hasNextLine() && definition == null) {
-			line = sc.nextLine();
+		if (map.containsKey(word.toUpperCase())) {
+			if (map.get(word.toUpperCase()) != "null")
+				definition = map.get(word.toUpperCase());
+		}
 
-			if (line.substring(0, word.length()).equalsIgnoreCase(word)
-					&& line.substring(word.length()).isEmpty() == false) {
-				definition = line.substring(word.length() + 1);
-			}
-		}
-		if (definition != null) {
-			if (definition.isEmpty() || definition == " ") {
-				definition = null;
-			}
-		}
-		sc.close();
 		return definition;
 	}
 }
